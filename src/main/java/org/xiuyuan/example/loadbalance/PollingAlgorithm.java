63866c99c1adb9527e6b0be10ba27ae100430ba2
xiuyuan <xiuyuan@pinduoduo.com>
2018-09-28 14:05:50 +0800

    modify the description.

--- src/main/java/org/xiuyuan/example/loadbalance/PollingAlgorithm.java
+++ src/main/java/org/xiuyuan/example/loadbalance/PollingAlgorithm.java
@@ -4,7 +4,10 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * 轮训算法, 将请求按照顺序轮流的分配到服务器上, 均衡对待每一台后端的服务器, 不关心服务器的连接数和负载情况
+ * <p>polling algorithm, dispatch the request to server as the request order,
+ * it treat all the server equality, no consider of the server's current connections
+ * and the load.
+ * </p>
  *
  * @author ray
  * created by helanzhou
@@ -14,7 +17,7 @@ import java.util.List;
 public class PollingAlgorithm {
 
     public static List<String> serverList = Arrays.asList("192.168.0.1", "192.168.0.2", "192.168.0.3", "192.168.0.4",
-                                                          "192.168.0.5");
+                                                          "192.168.0.5", "192.168.0.6", "192.168.0.7", "192.168.0.8");
 
     public static int pos = 0;
 
@@ -32,7 +35,7 @@ public class PollingAlgorithm {
     }
 
     public static void main(String[] args) {
-        for (int i=0; i<10; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.println(PollingAlgorithm.getServer());
         }
     }
