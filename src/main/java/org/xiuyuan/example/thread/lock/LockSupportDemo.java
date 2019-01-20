c6921673e6bb770048f42c3df3cc6d8150dba163
xiuyuan <xiuyuan@pinduoduo.com>
2019-01-20 23:23:15 +0800

    modify the two class implementation.

--- src/main/java/org/xiuyuan/example/thread/lock/LockSupportDemo.java
+++ src/main/java/org/xiuyuan/example/thread/lock/LockSupportDemo.java
@@ -9,7 +9,24 @@ import java.util.concurrent.locks.LockSupport;
  */
 public class LockSupportDemo {
 
-    public static void main(String[] args) {
+    public static Object obj = new Object();
+
+    static ChangeObjectThread t1 = new ChangeObjectThread("t1");
+    static ChangeObjectThread t2 = new ChangeObjectThread("t2");
+
+    public static void main(String[] args) throws InterruptedException{
+        t1.start();
+        Thread.sleep(100);
+        t2.start();
+        LockSupport.unpark(t1);
+        LockSupport.unpark(t2);
+        t1.join();
+        t2.join();
+
+        System.out.println("thread end");
+    }
+
+    public static void testPerson() {
         Person person = new Person();
 
         Thread thread = new Thread(() -> {
@@ -27,6 +44,20 @@ public class LockSupportDemo {
         LockSupport.unpark(thread);
     }
 
+    public static class ChangeObjectThread extends Thread {
+
+        public ChangeObjectThread(String name) {
+            super.setName(name);
+        }
+
+        @Override
+        public void run() {
+            synchronized (obj) {
+                System.out.println("in " + getName());
+                LockSupport.park();
+            }
+        }
+    }
 
 }
 
